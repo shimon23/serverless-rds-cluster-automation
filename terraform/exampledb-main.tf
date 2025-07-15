db43f4d0a53c59ff84d7b20408b49ebf3b083f2b
Shimon Hagag <33640542+shimon23@users.noreply.github.com>
2025-07-15 23:41:07 +0300

    Add Terraform for exampledb

---
+++ terraform/exampledb-main.tf
@@ -0,0 +1,19 @@
+
+                    resource "aws_db_instance" "exampledb" {
+                    identifier              = "exampledb-instance"
+                    allocated_storage       = 20
+                    engine                  = "mysql"
+                    engine_version          = "8.0"
+                    instance_class          = "db.t3.micro"
+                    db_name                 = "exampledb"
+                    username                = "admin"
+                    password                = "PLACEHOLDER_PASSWORD"
+                    skip_final_snapshot     = true
+                    publicly_accessible     = true
+
+                    tags = {
+                        Name        = "exampledb-instance"
+                        Environment = "dev"
+                    }
+                    }
+                    
